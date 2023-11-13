ccf7c198631cf45f24ea3ab533108dd457504a7c
Istiak Shaharia <38140617+imIstiak@users.noreply.github.com>
2023-11-13 18:08:20 +0600

    Final Update

--- expression.cpp
+++ expression.cpp
@@ -1,39 +1,77 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+#include<fstream>
+
 using namespace std;
 
 bool isOperator (char a){
-           return (a == '+' || a == '-' || a == '*' || a =='/' || a=='=');
-           
+           return (a == '+' || a == '-' || a == '*' || a =='/'|| a == '=');
 }
+bool isPunctuation(char a) {
+    return ( a==';' || a==',');}
+
+bool isConstant(char c) {
+  return (isdigit(c) || c == '.' || c == '-' /*|| c == '+' || c == '='*/);
+}
+
+void listPunctuation(string myText) {
+    cout <<" Punctuation Expression: ";
+    for (int i = 0; i < myText.length(); i++) {
+        if (isPunctuation(myText[i])) {
+            cout << myText[i] << " ";
+        }
+    }
+    cout << endl;
+}
+
 
 
 void listOperators (string myText)
 {
-    cout<<" operators: ";
+    cout<<" Arithmetic Expression: ";
 
     for (int i=0; i<myText.length();i++)
     {
         if (isOperator(myText[i])) {
             cout<< myText [i]<<" ";
         }
-    }
+
+        }
+
+
     cout<<endl;
 }
 
-int main () {
+void listConstant (string myText)
+{
+    cout<<" Constants: ";
+    for (int i = 0; i < myText.length(); i++) {
+      if (isConstant(myText[i])) {
+        cout << myText[i] << " ";
+      }
+    }
+
+cout<<endl;
+
+}
+
+int main()
+{
 
   string myText;
 
+
   // Read from the text file
-  ifstream MyReadFile("isti.txt");
+  ifstream MyReadFile;
+  MyReadFile.open("isti.txt");
 
   // Use a while loop together with the getline() function to read the file line by line
   while (getline (MyReadFile, myText)) {
-         listOperators (myText);
     // Output the text from the file
-   /* cout << myText; */
+    listOperators (myText);
+    listPunctuation(myText);
+    listConstant(myText);
+
+    //cout << myText;
   }
 
   // Close the file
